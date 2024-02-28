5c8867175fa27c1b0417989a9dc1e7cace4cbaab
Gabriel Martins dos Santos <gabriel.santos@PORTOVELHO.RO.GOV.BR>
2024-02-28 12:58:48 -0400

    feat: update

--- 3-sheet_read.py
+++ 3-sheet_read.py
@@ -2,4 +2,10 @@ from openpyxl import load_workbook
 
 # 1-Lê pasta de trabalho e planilha
 wb = load_workbook("data/pivot_table.xlsx")
-sheet = wb[""]
+sheet = wb["Relatório"]
+
+# 2-Acessando um valor específico
+print(sheet["A3"].value)
+print(sheet["B3"].value)
+
+# 3-
