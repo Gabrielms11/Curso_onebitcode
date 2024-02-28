dc2735d34eb8a510eff2b06d5d6ac87fc9171647
Gabriel Martins dos Santos <gabriel.santos@PORTOVELHO.RO.GOV.BR>
2024-02-28 13:17:03 -0400

    feat: update

--- 3-sheet_read.py
+++ 3-sheet_read.py
@@ -8,4 +8,9 @@ sheet = wb["Relatório"]
 print(sheet["A3"].value)
 print(sheet["B3"].value)
 
-# 3-
+# 3-Iterando valores por meio de loop
+for i in range(2, 6):
+    ano = sheet["A%s" % i].value
+    am = sheet["B%s" % i].value
+    bt = sheet["C%s" % i].value
+    print("{0} o Aston Martin vendeu {1} e o Bentley vendeu {2}".format(ano, am, bt))
